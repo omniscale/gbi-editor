5286acc3a4e02cfc11b5dc5887cc1d3c240bef6b
Kai Culemann <culemann@omniscale.de>
2014-01-09 15:35:40 +0100

    Extend ImprovedSelectFeature Control

--- src/openlayers.extensions/improved_select_feature.js
+++ src/openlayers.extensions/improved_select_feature.js
@@ -20,14 +20,45 @@ OpenLayers.Control.ImprovedSelectFeature = OpenLayers.Class(OpenLayers.Control.S
         var start = new Date().getTime();
         for(var l=0; l<layers.length; ++l) {
             layer = layers[l];
-            layer.events.triggerEvent("start_featuresunselecting", {layer: layer});
+            layer.events.triggerEvent("start_bulk", layer);
             for(var i=layer.selectedFeatures.length-1; i>=0; --i) {
                 feature = layer.selectedFeatures[i];
                 if(!options || options.except != feature) {
                     this.unselect(feature);
                 }
             }
-            layer.events.triggerEvent("finished_featuresunselecting", layer);
+            layer.events.triggerEvent("end_bulk", layer);
+        }
+    },
+    affectedLayers: function(features) {
+        var layers = [];
+        for(var i = 0; i < features.length; i++) {
+            var found = false;
+            for(var j = 0; j < layers.length; j++) {
+                if(layers[j] == features[i].layer) {
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) {
+                layers.push(features[i].layer)
+            }
+        }
+        return layers;
+    },
+    selectFeatures: function(features) {
+        var layers = this.affectedLayers(features);
+        for(var i = 0; i < layers.length; i++) {
+            layers[i].events.triggerEvent("start_bulk", layers[i]);
+        }
+        for(var i=0; i<features.length;i++) {
+            if(OpenLayers.Util.indexOf(features[i].layer.selectedFeatures, features[i]) != -1) {
+                this.unselect(features[i])
+            }
+            this.select(features[i]);
+        }
+        for(var i = 0; i < layers.length; i++) {
+            layers[i].events.triggerEvent("end_bulk", layers[i]);
         }
     },
     CLASS_NAME: "OpenLayers.Control.ImprovedSelectFeature"
